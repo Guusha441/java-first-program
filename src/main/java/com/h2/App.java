031cb2147663520fbc9eefd71d8bdd7d97168b2d
Guusha441 <66376157+Guusha441@users.noreply.github.com>
2022-12-15 00:35:36 +0100

    Finding best loanRate

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -2,16 +2,23 @@ package com.h2;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number * number;
+    }
+
+    private static int add(int[] number) {
+        int sum =0;
+        for (int i = 0; i < number.length; i++) {
+            sum+= number[i];
+
+        }
+        return sum;
     }
 }
