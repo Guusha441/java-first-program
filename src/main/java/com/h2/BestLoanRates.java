bb15570b5fd2197904b87f63a10384e06045bcb0
Guusha441 <66376157+Guusha441@users.noreply.github.com>
2022-12-15 23:06:32 +0100

    Finding best loanRate

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -4,38 +4,33 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class BestLoanRates {
-    public static final Map<Integer, Float> bestRates = Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
-
-    {
-
-
-    }
-
-    public static float getRates(int loanTermInYears) {
-        if (bestRates.containsKey(loanTermInYears)) {
-            return loanTermInYears;
-        } else {
-            return 0.0f;
-        }
-    }
+    public static final Map<Integer, Float> bestRates = Map.of(
+            1, 5.50f,
+            2, 3.45f,
+            3, 2.67f);
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name");
         String name = scanner.nextLine();
         System.out.println("Hello " + name);
-        System.out.println("Enter the loan term (in years)");
-        int loanTermInYears = scanner.nextInt();
-        getRates(loanTermInYears);
-       float bestRate =loanTermInYears;
-       if (bestRate== 0.0f){
-           System.out.println("No available rates for term: " + loanTermInYears + " years");
-       }else {
-           System.out.println("Best Available Rate: " + getRates(loanTermInYears) + "%");
-       }
 
-
-
-
+        System.out.println("Enter the loan ter  (in Years)");
+        int loanTermsInYears = scanner.nextInt();
+        float bestRate = getRates(loanTermsInYears);
+        if (bestRate == 0.0f) {
+            System.out.println("No available rates for terms " + loanTermsInYears + "years");
+        } else {
+            System.out.println("Best available rates " + getRates(loanTermsInYears) + "%");
+        }
+        scanner.close();
     }
+
+    private static float getRates(int loanTermsInYears) {
+        if (bestRates.containsKey(loanTermsInYears)) {
+            return bestRates.get(loanTermsInYears);
+        }
+        return 0.0f;
+    }
+
 }
